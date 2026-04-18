d2e8f8e91e0013883fda3ab6371358e42a144588
anasmorwan <anasmorwan5@gmail.com>
2026-04-18 19:25:08 +0200

    Update drive.py

--- project/drive.py
+++ project/drive.py
@@ -1,1 +1,16 @@
+import requests
+from config import IMAGE_FOLDER
+import os
 
+
+def download_image(url, filename="image.jpg"):
+    if not os.path.exists(IMAGE_FOLDER):
+        os.makedirs(IMAGE_FOLDER)
+
+    path = os.path.join(IMAGE_FOLDER, filename)
+
+    r = requests.get(url)
+    with open(path, "wb") as f:
+        f.write(r.content)
+
+    return path
